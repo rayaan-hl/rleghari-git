a02f95930f39454943ae557175f634743312b50d
Syed Ali Bokhari <sbokhari@Alis-MBP.lan>
2024-01-23 16:34:27 -0500

    Test commit from run.sh script

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ git add . > /dev/null 2>&1
 git commit -m "Test commit from run.sh script" > /dev/null 2>&1
 git push > /dev/null 2>&1
 
-echo "Push has been completed GANDU. This is a super simple BASH script. Open to run.py to understand what is going on"
+echo "Push has been completed GANDU. This is a super simple BASH script. Open to run.py to understand what is going on!!"
 
