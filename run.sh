b9d351b82c9f35f5686319f11e86335f41a505a5
Rleghariconestoga <Rleghari3926@conestogac.on.ca>
2024-01-23 16:52:48 -0500

    Test commit from run.sh script

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ echo "Pushing your changes from local to origin"
 
 git add . > /dev/null 2>&1
 git commit -m "Test commit from run.sh script" > /dev/null 2>&1
-git push 
+git push > /dev/null 2>&1
 
 echo "Push has been completed GANDU. This is a super simple BASH script. Open to run.py to understand what is going on!!"
 
